9ea16e4e7f49b1353101f781b912331f48c03e9b
Your Name <you@example.com>
2024-03-13 13:00:21 +0200

    first commit

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -8,13 +8,13 @@ var routes = require('./routes');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-var MONGODB_URI = 'mongodb+srv://seifadelelpop:sJQYK6JNO21tVl3G@cluster0.18jvlze.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+var MONGODB_URI = 'mongodb://localhost:27017/med-api';
 mongoose.connect(MONGODB_URI).then(function () {
   console.log('Connected to MongoDB');
   app.use(express.json());
   app.use('/api', routes);
   app.listen(PORT, function () {
-    return console.log('Server is running on port 3000');
+    return console.log('the server running on 3000');
   });
 })["catch"](function (err) {
   return console.error('Error connecting to MongoDB:', err);
